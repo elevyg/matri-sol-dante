847f78ee6e257a14ff8f32ea9be7795b3b1ad3ef
elevyg <elevyg91@gmail.com>
2021-10-12 11:55:06 -0300

    fix: db settings

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,3 +1,17 @@
 import { getFirestore } from "firebase/firestore";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDyADnlQyC06ZRZsq1pYa8n3J8oUKnVOwU",
+  authDomain: "matri-de483.firebaseapp.com",
+  projectId: "matri-de483",
+  storageBucket: "matri-de483.appspot.com",
+  messagingSenderId: "205407944996",
+  appId: "1:205407944996:web:45a1f679b8b8e3b70b73b1",
+  measurementId: "G-NLGG0RMLDY",
+};
+
+const app = initializeApp(firebaseConfig);
+getAnalytics(app);
 export const db = getFirestore();
