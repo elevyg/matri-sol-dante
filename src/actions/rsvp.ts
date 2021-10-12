58ee2e6e62c713cec5059893c55f658b450a5a76
Eyal Levy <elevyg91@gmail.com>
2021-10-12 18:37:03 -0300

    V2.2 (#3)
    * feat: guest list

--- src/actions/rsvp.ts
+++ src/actions/rsvp.ts
@@ -1,9 +1,16 @@
 /* eslint-disable consistent-return */
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, getDocs } from "firebase/firestore";
 import { db } from "../utils/db";
 
 export const addRSVP = async (data) => {
   const docRef = await addDoc(collection(db, "rsvp"), data);
 
   return docRef;
+};
+
+export const readRSVP = async () => {
+  const docRef = await getDocs(collection(db, "rsvp"));
+  const res = [];
+  docRef.forEach((doc) => res.push({ id: doc.id, ...doc.data() }));
+  return res;
 };
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,2 +1,3 @@
 export { default as Home } from "./Home";
 export { default as BasicInfo } from "./BasicInfo";
+export { default as GuestList } from "./GuestList";
