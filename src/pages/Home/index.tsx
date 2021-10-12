33dc6bbc5d158a451b6063d8399af297f13676c0
elevyg <elevyg91@gmail.com>
2021-10-12 02:19:41 -0300

    fix: clg deleted

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -6,16 +6,13 @@ import Hero from "../../components/Hero";
 import Footer from "../../components/Footer";
 import RSVP from "../../components/RSVP";
 
-const Home = () => {
-  console.log("hola");
-  return (
-    <div>
-      <Hero />
-      <RSVP />
-      <Invitation />
-      <Footer />
-    </div>
-  );
-};
+const Home = () => (
+  <div>
+    <Hero />
+    <RSVP />
+    <Invitation />
+    <Footer />
+  </div>
+);
 
 export default Home;
