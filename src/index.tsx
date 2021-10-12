81b6844f4b3da4385ed7ded7c3a188d5a19e8fb4
elevyg <elevyg91@gmail.com>
2021-10-12 03:13:14 -0300

    fix: firebase implementation

--- src/index.tsx
+++ src/index.tsx
@@ -7,8 +7,7 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+
 
 import { QueryClient, QueryClientProvider } from "react-query";
 import esLocale from "date-fns/locale/es";
@@ -17,18 +16,6 @@ import "./index.css";
 import { theme } from "./utils/theme";
 import AppRouter from "./router/AppRouter";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDyADnlQyC06ZRZsq1pYa8n3J8oUKnVOwU",
-  authDomain: "matri-de483.firebaseapp.com",
-  projectId: "matri-de483",
-  storageBucket: "matri-de483.appspot.com",
-  messagingSenderId: "205407944996",
-  appId: "1:205407944996:web:45a1f679b8b8e3b70b73b1",
-  measurementId: "G-NLGG0RMLDY",
-};
-
-const app = initializeApp(firebaseConfig);
-getAnalytics(app);
 
 const queryClient = new QueryClient();
 
