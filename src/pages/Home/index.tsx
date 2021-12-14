d34c65c48604d6bd3fe08424bb6ff7dc7ed74b92
elevyg <elevyg91@gmail.com>
2021-12-14 09:49:26 -0300

    chore: RSVP removed

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,14 +4,14 @@ import Invitation from "../../components/Invitation";
 import Hero from "../../components/Hero";
 
 import Footer from "../../components/Footer";
-import RSVP from "../../components/RSVP";
+
 import Gifts from "../../components/Gifts";
 import { Image } from "../../components/atoms";
 
 const Home = () => (
   <div>
     <Hero backgroundImage="https://lacamiyeyalsecasan.s3.us-west-2.amazonaws.com/loa-circulo.jpg" />
-    <RSVP />
+
     <Image
       src="https://lacamiyeyalsecasan.s3.us-west-2.amazonaws.com/loa-pampa.jpg"
       alt="loa"
